d9604d0abffbd7da869c77b5705ad26fe021eb35
hiromia006 <hiromia006@gmail.com>
2026-06-24 22:06:21 +0600

    add ParaBankUtil class and update .gitignore to exclude build directory

---
+++ src/test/java/com/parabank/parasoft/util/ParaBankUtil.java
@@ -0,0 +1,15 @@
+package com.parabank.parasoft.util;
+
+public class ParaBankUtil {
+    public static final int WAIT_TIME = 30;
+    public static final String ERROR = "Please enter a username and password.";
+
+    public static void waitForDomStable(){
+        try {
+            Thread.sleep(1500);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
